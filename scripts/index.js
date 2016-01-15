270a708f71e93193359dffa2c9dd2c6ac10262f3
Richard Roncancio <batusai513@msn.com>
2016-01-14 22:22:20 -0500

    Update tools, babel 6, react 0.14, finally working

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var App = require("./components/app");
 
 var app = React.createFactory(App);
-module.exports = React.render(app(), document.getElementById('container'));
+module.exports = ReactDOM.render(app(), document.getElementById('container'));
